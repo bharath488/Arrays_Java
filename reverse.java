e2b78ecdd8fb19bc9fd124f344ae178b83b29fb8
bharath488 <22hr1a05b2@mtieat.org>
2025-07-05 13:48:44 +0530

    Create reverse.java

---
+++ reverse.java
@@ -0,0 +1,9 @@
+class reverse{
+public static void main(String args[]){
+int arr[]={9,8,7,6,5,4,3,2,1};
+System.out.print("The reverse order of an array:");
+for(int i=arr.length-1;i>=0;i--){
+System.out.print(arr[i]+" ");
+}
+}
+}
