4f99404d8d4f04762a16bb1f57d9ff3661972978
bharath488 <22hr1a05b2@mtieat.org>
2025-07-05 18:25:50 +0530

    Create index.java

---
+++ index.java
@@ -0,0 +1,12 @@
+class index{
+public static void main(String args[]){
+int arr[]={10,20,30,40,50,60};
+int target=60;
+System.out.print("Index of a given number is :");
+for(int i=0;i<arr.length;i++){
+if(arr[i]==target){
+System.out.print(i);
+}
+}
+}
+}
