927ddd286421e3c7fe08161cb3885ef2c88b4868
bharath488 <22hr1a05b2@mtieat.org>
2025-07-05 13:00:05 +0530

    Create Search.java

---
+++ Search.java
@@ -0,0 +1,11 @@
+class Search{
+public static void main(String args[]){
+int arr[]={5,6,7,8,2};
+int target=2;
+for(int i=0;i<arr.length;i++){
+if(arr[i]==2){
+System.out.println(arr[i]);
+}
+}
+}
+}
