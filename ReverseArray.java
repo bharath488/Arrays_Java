5aa929284fdaf9928d38d8bc07250200f66e04fa
bharath488 <22hr1a05b2@mtieat.org>
2025-07-16 23:27:51 +0530

    Create ReverseArray.java

---
+++ ReverseArray.java
@@ -0,0 +1,9 @@
+class ReverseArray{
+  public static void main(String[] args){
+    int arr[]={9,5,2,7,1,10};
+    System.out.println("Reverse of array:");
+    for(int i=arr.length-1;i>=0;i--){
+      System.out.print(arr[i]+" ");
+    }
+  }
+}
