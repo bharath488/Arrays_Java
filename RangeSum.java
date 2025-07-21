8d522e18ad8480ae97c378c2f45e2bacefb7b4dc
bharath488 <22hr1a05b2@mtieat.org>
2025-07-21 22:24:58 +0530

    Update RangeSum.java

--- RangeSum.java
+++ RangeSum.java
@@ -1,25 +1,22 @@
 import java.util.*;
-class main{
-  public static void main(String[] args){
-    Scanner sc=new Scanner(System.in);
-    System.out.println("Enter the size:");
-    int n=sc.nextInt();
-    System.out.println("Enter the array:");
-    int arr[]=new int[n];
-    for(int i=0;i<n;i++){
-      arr[i]=sc.nextInt();
+class Main{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the size of array:");
+        int n=sc.nextInt();
+        System.out.println("Enter array:");
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        System.out.println("Enter first range");
+        int start=sc.nextInt();
+        System.out.println("Enter second range");
+        int end=sc.nextInt();
+        int rangeSum=0;
+        for(int i=start;i<=end;i++){
+            rangeSum+=arr[i];
+        }
+        System.out.println("Rangesum of an array is: "+ rangeSum);
     }
-    System.out.println("Enter the first range element:");
-    int l=sc.nextInt();
-    System.out.println("Enter the second range element:");
-    int r=sc.nextInt();
-    int sum=0;
-    if(l<0 || r>=n || l>r){
-      System.out.println("Element not found");
-    }
-    for(int i=l;i<r+1;i++){
-      sum+=arr[i];
-    }
-    System.out.println("Range sum of array is :"+sum);
-  }
 }
